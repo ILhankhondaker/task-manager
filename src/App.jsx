12a1a7462f8d1aba9765e236506a4633d2a6a06b
ILhankhondaker <ilhankhondaker@gmail.com>
2024-01-17 01:17:31 +0600

    add header footer & introduce section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import Footer from "./Pages/Footer"
+import Header from "./Pages/Header"
+import Introduce from "./Pages/Introduce"
 
 function App() {
  
   return (
     <>
     <h1 className="text-center">Start New Project for To Do List </h1>
+    <Header></Header>
+    <Introduce></Introduce>
+    <Footer></Footer>
     </>
   )
 }
